b9bcd9674ed04ccbc37dd266daa686a8f0f4c62b
EQuezada <EQuezada@student.neumont.edu>
2017-05-24 16:45:25 -0600

    Solitaire empty methods

---
+++ Solitaire.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solitaire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public void MakeBoard()
+        {
+
+        }
+        public void ShowBoard()
+        {
+
+        }
+        public void PickAndPlaceCard()
+        {
+
+
+        }
+        public void DrawCard()
+        {
+
+        }
+        public void StackGroupOfCards()
+        {
+
+        }
+        public void WinOrLose()
+        {
+
+        }
+
+        public void Restart()
+        {
+
+        }
+        public void Exit()
+        {
+
+        }
+
+    }
+}
