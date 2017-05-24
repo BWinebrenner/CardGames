7f7fb881d12bd3c95d7dbd052ff33dd5d82563ae
Daniel <DGingras@student.neumont.edu>
2017-05-24 15:59:55 -0600

    blackjack with little code

--- blackjack/blackjack/Program.cs
+++ blackjack/blackjack/Program.cs
@@ -6,11 +6,37 @@ using System.Threading.Tasks;
 
 namespace blackjack
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
 
+        }
+        public static void Hit()
+        {
+
+        }
+        public static void Stay()
+        {
+
+        }
+        public static void AI()
+        {
+
+        }
+        public static void Deal()
+        {
+
+        }
+        public static void Driver()
+        {
+            bool logic = true;
+            Hit();
+            Hit();
+            while (logic)
+            {
+                
+            }
         }
     }
 }
