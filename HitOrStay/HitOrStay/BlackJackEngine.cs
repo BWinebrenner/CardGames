d96f0ed4219480f7521f88d59bfdf2a5298a6290
Daniel <DGingras@student.neumont.edu>
2017-06-02 15:24:44 -0600

    re pushing

---
+++ HitOrStay/HitOrStay/BlackJackEngine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Deck1;
+using Card1;
+
+namespace HitOrStay
+{
+    public class BlackJackEngine
+    {
+        private static Deck playing = new Deck();
+        public static void Gamestart()
+        {
+            playing.Shuffle();
+
+        }
+        public static Card Hit()
+        {
+            Card delt = playing.Dealer();
+            return delt;
+        }
+        public static void Stay()
+        {
+
+        }
+        public static void AI()
+        {
+
+        }
+        public static void Player()
+        {
+            int cardtotal = 0;
+
+        }
+    }
+}
