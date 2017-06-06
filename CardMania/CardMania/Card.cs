f94ec5e7fc2b4f54e0153d176c746284f35369e1
Daniel <DGingras@student.neumont.edu>
2017-06-06 14:55:37 -0600

    saved blackjack as dll

---
+++ CardMania/CardMania/Card.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card1
+{
+    public class Card
+    {
+        public enum SUITVALUE { spades, hearts, clubs, dimaonds }
+        public enum FACEVALUE { two, three, four, five, six, seven, eight, nine, ten, jack, queen, king, ace }
+        private FACEVALUE faceValue;
+        private SUITVALUE suitValue;
+
+
+        public Card(FACEVALUE faceValue, SUITVALUE suitValue)
+        {
+            this.faceValue = faceValue;
+            this.suitValue = suitValue;
+        }
+
+        public FACEVALUE getFaceValue()
+        {
+            return faceValue;
+        }
+
+        public SUITVALUE getSuitValue()
+        {
+            return suitValue;
+        }
+
+
+        public override string ToString()
+        {
+            return this.faceValue + " of " + this.suitValue;
+        }
+    }
+}
+
