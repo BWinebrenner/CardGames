4fc4c1847db3e8c07a652c4885cb5634a8b8093f
Daniel <DGingras@student.neumont.edu>
2017-06-06 14:39:15 -0600

    adding all programs to one file

--- CardMania/CardMania/Program.cs
+++ CardMania/CardMania/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Card1;
+using Decks;
+using consoleblackjack;
+
 
 namespace CardMania
 {
