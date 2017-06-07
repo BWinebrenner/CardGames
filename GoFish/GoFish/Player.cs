b8fec605b95ef04b45b90911272176a84a35a9b7
EQuezada <EQuezada@student.neumont.edu>
2017-06-07 12:45:22 -0600

    Add files via upload
    Gofish from github

---
+++ GoFish/GoFish/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoFish
+{
+    class Player
+    {
+        private string name;
+        private int cardsInHand;
+
+        public Player(String name, int cardInHand)
+        {
+            this.name = name;
+            this.cardsInHand = cardsInHand;
+
+        }
+        public string getName()
+        {
+            return name;
+        }
+
+        public int getCardInHand()
+        {
+            return cardsInHand;
+        }
+
+
+    }
+}
--- GoFish/GoFish/Program.cs
+++ GoFish/GoFish/Program.cs
@@ -8,42 +8,76 @@ namespace GoFish
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
+            
         }
 
-        static void ToatalAmountOfPlayers()
+        static void ShuffleDeck()
         {
+            Deck deck = new Deck();
+            deck.Shuffle();
 
         }
 
-        static void CvP()
+        static void GivingCards()
         {
+            Deck deal = new Deck();
+            deal.Dealer();
+            
 
         }
 
-        static void PlayerHands()
-        {
-
-        }
-
-        static void Match()
-        {
-
-        }
-
-        static void Draw()
-        {
-
-        }
-
-        static void AI()
-        {
-
-        }
+        
 
 
         
+        static void MatchCards()
+        {
+          
+            Card.FACEVALUE cardTwo = Card.FACEVALUE.two;
+            int value0 = (int)cardTwo;
+
+            Card.FACEVALUE cardThree = Card.FACEVALUE.three;
+            int value1 = (int)cardThree;
+
+            Card.FACEVALUE cardFour = Card.FACEVALUE.four;
+            int value2 = (int)cardFour;
+
+            Card.FACEVALUE cardFive = Card.FACEVALUE.five;
+            int value3 = (int)cardFive;
+
+            Card.FACEVALUE cardSix = Card.FACEVALUE.six;
+            int value4 = (int)cardSix;
+
+            Card.FACEVALUE cardSeven = Card.FACEVALUE.seven;
+            int value5 = (int)cardSeven;
+
+            Card.FACEVALUE cardEight = Card.FACEVALUE.eight;
+            int value6 = (int)cardEight;
+
+            Card.FACEVALUE cardNine = Card.FACEVALUE.nine;
+            int value7 = (int)cardNine;
+
+            Card.FACEVALUE cardTen = Card.FACEVALUE.ten;
+            int value8 = (int)cardTen;
+
+            Card.FACEVALUE cardJack = Card.FACEVALUE.jack;
+            int value9 = (int)cardJack;
+
+            Card.FACEVALUE cardQueen = Card.FACEVALUE.queen;
+            int value10 = (int)cardQueen;
+
+            Card.FACEVALUE cardKing = Card.FACEVALUE.king;
+            int value11 = (int)cardKing;
+
+            Card.FACEVALUE cardAce = Card.FACEVALUE.ace;
+            int value12 = (int)cardAce;
+
+           
+
+        }
      
     }
 }
